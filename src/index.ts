e09e5b23e1befeaf63f1590a4b170ef24579cc0b
metanju <juho.mettanen@elisanet.fi>
2026-06-16 14:59:09 +0300

    added word filttering and error handling on responses

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,30 @@
-import express, { NextFunction } from "express";
-import { Request, Response } from "express";
-import { config } from "./config.js";
+import express from "express";
+
+import { handlerReadiness } from "./api/readiness.js";
+import { handlerMetrics } from "./api/metrics.js";
+import { handlerReset } from "./api/reset.js";
+import { middlewareLogResponse, middlewareMetricsInc, } from "./api/middleware.js";
+import { handlerChirpsValidate } from "./api/validate_chirp.js"
+import { handlerErrors } from "./api/errors.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(middlewareLogResponses);
-app.use("/app", middlewareMetricsInc, express.static("./src/app"));
-app.use("/metrics", handlerMetrics, express.static("./src/app"))
-app.use("/reset", resetMetrics, express.static("./src/app"));
+app.use(middlewareLogResponse);
+app.use(express.json());
+
+app.use("/app", middlewareMetricsInc);
+app.use("/app", express.static("./src/app"));
+
+app.post("/api/validate_chirp", handlerChirpsValidate);
+app.get("/api/healthz", handlerReadiness);
+
+app.get("/admin/metrics", handlerMetrics);
+app.post("/admin/reset", handlerReset);
+
+// MUST be last
+app.use(handlerErrors);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-app.get("/healthz", handleReadiness);
-
-function handleReadiness(_req: Request, res: Response) {
-    res.set("Content-Type", "text/plain; charset=utf-8");
-    res.send("OK");
-}
-
-function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
-    res.on("finish", () => {
-        if (res.statusCode < 200 || res.statusCode >= 300) {
-            console.log(`[NON-OK] ${req.method} ${req.originalUrl} - Status: ${res.statusCode}`);
-        }
-    });
-
-    next();
-}
-
-function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
-    config.fileserverHits++;
-    next();
-}
-function handlerMetrics(req: Request, res: Response) {
-    res.set("Content-Type", "text/plain");
-    res.send(`Hits: ${config.fileserverHits}`);
-}
-function resetMetrics(req: Request, res: Response) {
-    res.set(config.fileserverHits = 0);
-    res.send(`Hits reset`);
-}
